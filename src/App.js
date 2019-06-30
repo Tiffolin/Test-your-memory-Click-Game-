d4d16b388908c07037eaa1ef7f1b8c58aaa422ac
Tiffo <szuchinlin3@gmail.com>
2019-06-29 22:34:09 -0400

    deployed on Heroku

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-    
 import React, { Component } from 'react';
 import Navbar from "./components/Navbar";
 import Score from "./components/Score";
@@ -10,41 +9,34 @@ import './style.css';
 
 class App extends Component {
   state = {
-    imgs,
-    clicked: "",
-    count: 0,
-    topscore: 0
+            imgs,
+            clicked: "",
+            count: 0,
+            topscore: 0
   }
 
   CardPlus = id => {
-    // console.log(id.id);
+              // score 
+              this.setState({
+                count: this.state.count + 1,
+                clicked: id.id
+              });
 
-// score counter 
-    this.setState({
-      count: this.state.count + 1,
-      clicked: id.id
-    });
+              //shuffles image cards (found this method online)
+              function shuffle(array) {array.sort(() => Math.random() - 0.5);}
 
-    // card shuffler (items  in an array) - found this method online
-    function shuffle(array) {
-      array.sort(() => Math.random() - 0.5);
-    }
-      shuffle(imgs);
-      this.setState({imgs: imgs});
+              shuffle(imgs);
+               
+              this.setState({imgs: imgs});
 
-    // if same card id is clicked then restart score and record the topscore
-    if (this.state.clicked === id.id) {
-          if (this.state.count > this.state.topscore) {
-            this.setState({
-              topscore: this.state.count,
-            })
-          }
-          this.setState({
-            count: 0
-          })
-        }
+              // reset score to 0 if same card is clicked
+                  if (this.state.clicked === id.id) {
+                        if (this.state.count > this.state.topscore) {
+                            this.setState({topscore: this.state.count,})
+                           }
+                            this.setState({count:0})
+                  }
   }
-
 
   render() {
     return (
@@ -59,14 +51,13 @@ class App extends Component {
         <Wrapper>
         {this.state.imgs.map(img => (
           <Card
-            // removeFriend={this.removeFriend}
             id={img.id}
             key={img.id}
             image={img.image}
             CardPlus={this.CardPlus}
           />
         ))}
-      </Wrapper>
+        </Wrapper>
         {/* <Footer /> */}
       </div>
 
